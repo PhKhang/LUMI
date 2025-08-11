f1b026271b9df69416f2d2789534c63fec090e52
Phúc Khang <84757707+PhKhang@users.noreply.github.com>
2025-08-11 14:31:34 +0700

    feat: modification for next build

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,9 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 });
 
 const nextConfig: NextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   experimental: {
     optimizePackageImports: [
       "@chakra-ui/react",
